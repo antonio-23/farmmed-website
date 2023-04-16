b473dc3b182e79b536f80279921dc3d6540f99a9
Antoni Garczyński <antek.garczynski@gmail.com>
2023-04-16 16:04:58 +0200

    Poprawki w linkach do podstron
    Dodanie kluczy do elementów mapowania

--- frontend/src/components/Cards.jsx
+++ frontend/src/components/Cards.jsx
@@ -1,28 +1,26 @@
 import React from 'react';
 import { ImStarFull, ImStarHalf, ImStarEmpty } from 'react-icons/im';
 
-const propertis = [{ name: 'Barbara Nowak' }, { name: 'Piotr Kowalski' }, { name: 'Jan Kowalczyk' }];
+const properties = [{ name: 'Barbara Nowak' }, { name: 'Piotr Kowalski' }, { name: 'Jan Kowalczyk' }];
 
 export const Cards = () => {
   return (
     <>
-      {propertis.map((value) => (
-        <>
-          <div className='w-full flex flex-col p-4 my-4  bg-white border py-8 rounded-xl shadow-xl'>
-            <h2 className='text-2xl text-center py-8 font-bold'>{value.name}</h2>
-            <p className='py-2 mx-8 text-center text-gray-600'>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus incidunt eum dolores pariatur rerum omnis numquam atque dolorum optio similique suscipit et nam nostrum, hic, sunt debitis
-              deserunt expedita obcaecati.
-            </p>
-            <div className='flex justify-center gap-2 py-8'>
-              <ImStarFull color='#883AE1' />
-              <ImStarFull color='#883AE1' />
-              <ImStarFull color='#883AE1' />
-              <ImStarFull color='#883AE1' />
-              <ImStarHalf color='#883AE1' />
-            </div>
+      {properties.map((property) => (
+        <div key={property.name} className='w-full flex flex-col p-4 my-4 bg-white border py-8 rounded-xl shadow-xl'>
+          <h2 className='text-2xl text-center py-8 font-bold'>{property.name}</h2>
+          <p className='py-2 mx-8 text-center text-gray-600'>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus incidunt eum dolores pariatur rerum omnis numquam atque dolorum optio similique suscipit et nam nostrum, hic, sunt debitis deserunt
+            expedita obcaecati.
+          </p>
+          <div className='flex justify-center gap-2 py-8'>
+            <ImStarFull color='#883AE1' />
+            <ImStarFull color='#883AE1' />
+            <ImStarFull color='#883AE1' />
+            <ImStarFull color='#883AE1' />
+            <ImStarHalf color='#883AE1' />
           </div>
-        </>
+        </div>
       ))}
     </>
   );
